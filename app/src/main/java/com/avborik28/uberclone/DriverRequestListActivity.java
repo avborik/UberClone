801ba743a62a0507f58ec8fa876d69a3c16a3d95
AvBorikDeveloper <avboris28@gmail.com>
2020-04-15 00:41:00 +1200

    added activities settup cloud server, done the main activity layout and class

---
+++ app/src/main/java/com/avborik28/uberclone/DriverRequestListActivity.java
@@ -0,0 +1,14 @@
+package com.avborik28.uberclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DriverRequestListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_request_list);
+    }
+}
