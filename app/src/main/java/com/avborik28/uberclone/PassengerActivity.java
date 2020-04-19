b28552da20773f9900864f5a701d1c7cca0c220f
AvBorikDeveloper <avboris28@gmail.com>
2020-04-19 22:07:19 +1200

    Implemented the Passenger activity functions

--- app/src/main/java/com/avborik28/uberclone/PassengerActivity.java
+++ app/src/main/java/com/avborik28/uberclone/PassengerActivity.java
@@ -1,14 +1,189 @@
 package com.avborik28.uberclone;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class PassengerActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.parse.DeleteCallback;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseGeoPoint;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.util.List;
+
+public class PassengerActivity extends FragmentActivity implements OnMapReadyCallback, View.OnClickListener{
+
+    private GoogleMap mMap;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private Button btnRequestCar;
+    private Button btnBeepBeep;
+    private Button btnbtnLogoutFromPassengerActivity;
+    private boolean isUberCancelled = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passenger);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        btnRequestCar = findViewById(R.id.btnRequestCar);
+        btnBeepBeep = findViewById(R.id.btnBeepBeep);
+        btnbtnLogoutFromPassengerActivity = findViewById(R.id.btnLogoutFromPassengerActivity);
+
+        btnRequestCar.setOnClickListener(this);
+
+        ParseQuery<ParseObject> carRequestQuery = ParseQuery.getQuery("RequestCar");
+        carRequestQuery.whereEqualTo("username", ParseUser.getCurrentUser().getUsername());
+        carRequestQuery.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+                if(objects.size() > 0 && e == null){
+                    isUberCancelled = false;
+                    btnRequestCar.setText("Cancel the uber request");
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                updateCameraPassengerLocation(location);
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+       if(Build.VERSION.SDK_INT < 23) {
+           locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+       } else if(Build.VERSION.SDK_INT >= 23){
+           if (ContextCompat.checkSelfPermission(PassengerActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+               ActivityCompat.requestPermissions(PassengerActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1000);
+           } else {
+               locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+               Location currentPassengerLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+               updateCameraPassengerLocation(currentPassengerLocation);
+           }
+       }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode == 1000 && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED ){
+            if(ContextCompat.checkSelfPermission(PassengerActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                Location currentPassengerLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+                updateCameraPassengerLocation(currentPassengerLocation);
+            }
+        }
+    }
+    private void updateCameraPassengerLocation(Location pLocation){
+        LatLng passengerLocation = new LatLng(pLocation.getLatitude(),pLocation.getLongitude());
+        mMap.clear();
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(passengerLocation, 15));
+
+        mMap.addMarker(new MarkerOptions().position(passengerLocation).title("You are here"));
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(isUberCancelled == true){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            Location passengerCurrentLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+            if (passengerCurrentLocation != null) {
+                ParseObject requestCar = new ParseObject("RequestCar");
+                requestCar.put("username", ParseUser.getCurrentUser().getUsername());
+
+                ParseGeoPoint userLocation = new ParseGeoPoint(passengerCurrentLocation.getLatitude(), passengerCurrentLocation.getLongitude());
+
+                requestCar.put("passengerLocation", userLocation);
+
+                requestCar.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            Toast.makeText(PassengerActivity.this, "A car request was saved", Toast.LENGTH_LONG).show();
+
+                            btnRequestCar.setText("Cancel your uber order");
+                        }
+                    }
+                });
+            } else {
+                Toast.makeText(this, "Unknown error, Something went wrong", Toast.LENGTH_LONG).show();
+            }
+        }
+        } else {
+            ParseQuery<ParseObject> carRequestQuery = ParseQuery.getQuery("RequestCar");
+            carRequestQuery.whereEqualTo("username", ParseUser.getCurrentUser().getUsername());
+            carRequestQuery.findInBackground(new FindCallback<ParseObject>() {
+                @Override
+                public void done(List<ParseObject> requestList, ParseException e) {
+                    if(requestList.size() > 0 && e == null){
+                        isUberCancelled = true;
+                        btnRequestCar.setText("Request new uber");
+
+                        for(ParseObject uberRequest : requestList){
+                            uberRequest.deleteInBackground(new DeleteCallback() {
+                                @Override
+                                public void done(ParseException e) {
+                                    if(e == null){
+                                        Toast.makeText(PassengerActivity.this, "Request deleted",Toast.LENGTH_LONG);
+                                    }
+                                }
+                            });
+                        }
+                    }
+                }
+            });
+        }
     }
 }
